3f31b86bb014861f356116bf83690028af8de4cb
David <david.lu.singh@gmail.com>
2025-09-16 12:34:02 +0200

    click event handling no concrete implementation tho.....

--- main.c
+++ main.c
@@ -15,12 +15,8 @@ SDL_Renderer *ren;
 
 void preComputeGridCoordinates() {
     for (int x = 0; x < gridSize; ++x) {
-        for (int y = 0; y < gridSize; ++y) {
-            SDL_Point p;
-            p.x = border + x * offset;
-            p.y = border + y * offset;
-            gridCoords[x][y] = p;
-        }
+        for (int y = 0; y < gridSize; ++y)
+            gridCoords[x][y] = (SDL_Point) {border + x * offset, border + y * offset};
     }
 }
 
@@ -38,7 +34,7 @@ void drawGrid() {
     }
 }
 
-void drawStone(SDL_Point p, int color) {
+void drawStone(const SDL_Point p, const int color) {
     if (color == 1)
         SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
     else if (color == -1)
@@ -46,9 +42,9 @@ void drawStone(SDL_Point p, int color) {
 
     for (int x = p.x - stoneRadius; x <= p.x + stoneRadius; x++)
         for (int y = p.y - stoneRadius; y <= p.y + stoneRadius; y++) {
-            int dx = x - p.x;
-            int dy = y - p.y;
-            if (dx*dx + dy*dy <= stoneRadius*stoneRadius)
+            const int dx = x - p.x;
+            const int dy = y - p.y;
+            if (dx * dx + dy * dy <= stoneRadius*stoneRadius)
                 SDL_RenderDrawPoint(ren, x, y);
         }
 }
@@ -64,6 +60,10 @@ void drawStones() {
     }
 }
 
+void handleClick() {
+
+}
+
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
     win = SDL_CreateWindow("Go",SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, dim, dim, SDL_WINDOW_RESIZABLE);
@@ -76,6 +76,10 @@ int main() {
         while (SDL_PollEvent(&event) != 0) {
             if (event.type == SDL_QUIT)
                 quit = 1;
+            if (event.type == SDL_MOUSEBUTTONDOWN) {
+                if (event.button.button == SDL_BUTTON_LEFT)
+                    handleClick();
+            }
         }
 
         drawBackground();
